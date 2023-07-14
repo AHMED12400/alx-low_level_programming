11f1fc493ec687740f8a01871a75ba1fa80eb27f
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-14 06:45:34 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - display "what i will print"
+ * Return: 0
+ */
+int main (void)
+{	
+	printf ("/"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
