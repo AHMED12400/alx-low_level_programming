51daa89a6e3073a1545a26a03a7e551ea1004312
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-14 06:35:21 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-	#include <stdio.h>
-	/**
-	* main - show  This is my first program
-	* return:0 and exit the program
-	*/
-	int main(void)
+#include <stdio.h>
 
-{ 	
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+/**
+* main - show  This is my first program
+* return:0 and exit the program
+*/
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
 }
+
