a12b9259e962827653067f80154c2093912a1944
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-14 06:59:19 -0700

    -

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - display "what i will print"
- * Return: 0
+ * main - displays "what i will print" in new line
+ * Return: 0 and exit program
  */
-int main (void)
-{	
-	printf ("/"with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
