95d16d50dd3bc267176a4ad9ba01e8d932991428
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-14 06:38:08 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - show  This is my first program
-* return:0 and exit the program
-*/
+ * main - display  "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
+ */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
