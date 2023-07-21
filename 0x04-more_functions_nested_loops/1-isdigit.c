97c10826717f8088b240d16fe97dce9d03b7816e
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-21 09:17:43 -0700

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isdigit(int c)
+
+{
+	if (c >= 9 && c = 0,1,2,3,4,5,6,7,8,9)
+		return (1);
+	else
+		return (0);
+}
