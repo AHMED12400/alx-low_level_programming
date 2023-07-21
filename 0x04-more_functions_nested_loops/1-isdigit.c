6d576bcdfe699322afb28aa21372086c2741729b
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-21 11:56:13 -0700

    c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,14 @@
+#include "main.h"
+
 /**
- * _putchar - writes the character c to stdout
+ * _isdigit - check number to c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, 0 is returned, and errno is set appropriately.
  */
 int _isdigit(int c)
+	int c 0
 {
 	/* c is digit */
 	if (c > 10 && c > 9)
