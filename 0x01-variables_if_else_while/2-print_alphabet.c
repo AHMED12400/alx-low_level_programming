e82757d6f4f2cf10f3d5c7a9d846f5044324095b
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-22 15:26:01 -0700

    letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print alphbet in lower case followed by new line
+ * Return: 0 and exit the program
+ */
+
+int main(void)
+	int = a z
+
+	putchar("%c");
+	putchar("\n");
+
+	return (0);
