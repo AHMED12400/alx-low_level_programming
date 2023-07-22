e8ddc550fe783576623b5df53271ded1f254b73f
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-22 16:36:13 -0700

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits base on 10
+ * Return: 0 and exit the program
+ */
+
+int main(void)
+{
+        int number;
+
+        for (number = 0; number < 10; number++)
+        putchar(number);
+        putchar('\n');
+
+        return (0);
+}i
