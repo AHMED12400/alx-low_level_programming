000f66a855e90585408607aa597176a7b6674b00
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-22 16:57:29 -0700

    ll

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main print lowercase in reverse
+ * Return: 0 and exit program
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 'z'; l > 'a'; l++)
+		putchar(i);
+		putchar('\n');
+	return (0);
+}
