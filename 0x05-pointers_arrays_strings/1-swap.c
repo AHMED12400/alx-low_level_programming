523949c707bfe1d920c5e8547cc389952be9ce9f
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-24 16:57:47 -0700

    i

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0. and end
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	a = *b;
+	*a = c;
+	*a = *b
+	*b = *c;
+}
