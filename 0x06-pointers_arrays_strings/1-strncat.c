4b603645ef4de221d0e7da9ca3bceaa318408a83
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-27 16:52:33 -0700

    p

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, n;
+	int n = *src[j]
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; *src[j] = '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
