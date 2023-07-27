760c0c341dc1dab44d1d2b45aea6e1cfd183f3a3
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-27 15:39:00 -0700

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,11 @@ char *_strcat(char *dest, char *src)
 
 	while(dest[i]!='\0')
 	{
-	       i++
+	       i++;
 	}
 	while(src[j]!= '\0')
 	{
-	dest[i] = src[j]
+	dest[i] = src[j];
 	i++;
 	j++;
 	}
