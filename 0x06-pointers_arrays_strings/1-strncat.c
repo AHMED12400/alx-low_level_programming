67613e2e281ed0b250a5fd8f70a1538603e8b439
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-07-27 17:35:18 -0700

    p

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat 
+ * _strncat
  * @dest: char string
  * @src: char string
  * Return: pointer to resulting string `dest`
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0' &&n < 0; j++)
+	for (j = 0; src[j] != '\0' && n < 0; j++)
 	{
 		dest[i] = src[j];
 		i++;
