4a22028da5506a35fb6be73d890e50b73d3eba12
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-08-14 16:05:01 -0700

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+int main(void)
+
+{
+	struct dog dog;
+	dog.name = "Poppy";
+	dog.age = "3.5";
+	dog.owner = "Bob";
+	return (0);
+
+}
