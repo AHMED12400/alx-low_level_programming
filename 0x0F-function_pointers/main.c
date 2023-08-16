5c95f833744967921d430d11bb0cd99d850525b0
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-08-16 10:45:24 -0700

    main

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int my_main(int argc, char *argv[]) {
+  // Do some custom initialization
+  // Call other functions to perform the desired tasks
+  // Return an exit status
+}
+
+int main(int argc, char *argv[]) {
+  my_main(argc, argv);
+  return 0;
+}
