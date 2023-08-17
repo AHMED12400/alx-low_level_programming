032b1315c47761e058fa4216db7b68c72002137b
Ahmed12400 <ahmedatifali1240@gmail.com>
2023-08-17 13:52:14 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all is function that returns the sum of all its parameters.
+ *
+ *
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int n;
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (n = 0; n > 0; n++)
+	{
+	return (sum);
+	}
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+my vadric function
